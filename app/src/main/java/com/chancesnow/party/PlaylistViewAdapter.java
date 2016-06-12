21a6108469d0484c6442f74728c94c4694ba145d
Chance Snow <git@chancesnow.me>
2016-06-12 02:27:58 -0700

    Add icon to playlist items

--- app/src/main/java/com/chancesnow/party/PlaylistViewAdapter.java
+++ app/src/main/java/com/chancesnow/party/PlaylistViewAdapter.java
@@ -4,13 +4,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chancesnow.party.PlaylistFragment.OnPlaylistListListener;
+import com.squareup.picasso.Picasso;
 
 import java.text.NumberFormat;
 import java.util.List;
 
+import kaaes.spotify.webapi.android.models.Image;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
 
 /**
@@ -47,9 +50,19 @@ public class PlaylistViewAdapter extends RecyclerView.Adapter<PlaylistViewAdapte
                         numberFormat.format(mValues.get(position).tracks.total)
                 ));
 
+        // Download the icon, if available
+        String iconUrl = getLargestIcon(mValues.get(position).images, 500);
+        if (iconUrl != null) {
+            Picasso.with(holder.mView.getContext()).load(iconUrl).into(holder.mIcon);
+        }
+
+        holder.mView.setSelected(false);
+
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                v.setSelected(true);
+
                 if (null != mListener) {
                     // Notify the active callbacks interface (the activity, if the
                     // fragment is attached to one) that an item has been selected.
@@ -68,8 +81,29 @@ public class PlaylistViewAdapter extends RecyclerView.Adapter<PlaylistViewAdapte
         return mValues;
     }
 
+    private String getLargestIcon(List<Image> images, int max) {
+        int width = 0, height = 0;
+        String url = null;
+
+        if (images.size() > 0) {
+            for (Image image: images) {
+                if (image != null && image.width != null && image.height != null &&
+                        image.width < max && image.height < max &&
+                        (Math.max(width, image.width) == image.width ||
+                        Math.max(height, image.height) == image.height)) {
+                    width = image.width;
+                    height = image.height;
+                    url = image.url;
+                }
+            }
+        }
+
+        return url;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
+        public final ImageView mIcon;
         public final TextView mNameLabel;
         public final TextView mTrackCountLabel;
         public PlaylistSimple mItem;
@@ -77,6 +111,7 @@ public class PlaylistViewAdapter extends RecyclerView.Adapter<PlaylistViewAdapte
         public ViewHolder(View view) {
             super(view);
             mView = view;
+            mIcon = (ImageView) view.findViewById(R.id.playlist_icon);
             mNameLabel = (TextView) view.findViewById(R.id.playlist_name);
             mTrackCountLabel = (TextView) view.findViewById(R.id.playlist_trackCount);
         }
